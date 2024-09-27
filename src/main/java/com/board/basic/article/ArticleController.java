249042c87d4a690613600786b9118148a6f36c7c
JJk <wnsrb1594@naver.com>
2024-09-27 11:54:41 +0900

    2-2삭제구현

--- src/main/java/com/board/basic/article/ArticleController.java
+++ src/main/java/com/board/basic/article/ArticleController.java
@@ -61,5 +61,11 @@ public class ArticleController {
         return String.format("redirect:/article/detail/%s", id);
     }
 
+    @GetMapping("/delete/{id}")
+    public String articleDelete(@PathVariable("id")Integer id){
+        Article article = this.articleService.getArticle(id);
+        this.articleService.delete(article);
+        return "redirect:/";
+    }
 
 }
